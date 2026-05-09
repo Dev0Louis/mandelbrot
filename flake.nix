4fe8f1891274cb7bb8fcb8fce5a3cff195ae2889
LouisNotLuna <louis@trans.fish>
2026-05-09 12:47:36 +0200

    Nixify

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Rust NixOS Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust-toolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          	  
+          buildInputs = with pkgs; [
+	    rust-toolchain
+            rust-analyzer
+            rustfmt
+            clippy
+            lld
+	    wayland
+	    wayland-protocols
+	    egl-wayland
+	    libGL
+          ];
+  
+
+          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+	    wayland
+	    wayland-protocols
+	    egl-wayland
+	    libGL
+	  ];
+
+          RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
+} 
